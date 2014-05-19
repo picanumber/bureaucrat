e9378c5414a9db618972733f96c9928f575ac3b2
picanumber <picanumber@gmail.com>
2014-05-19 18:45:35 +0300

    Modified according to code review by Loki Astari

--- time_lapse.h
+++ time_lapse.h
@@ -20,13 +20,16 @@ namespace tim
 	template<typename TimeT = std::chrono::milliseconds>
 	struct measure
 	{
-		template<typename F>
-		static typename TimeT::rep execution(F const &func)
+		template<typename F, typename ...Args>
+		static typename TimeT::rep execution(F func, Args&&... args)
 		{
 			auto start = std::chrono::system_clock::now();
-			func();
+			
+			func(std::forward<Args>(args)...);
+
 			auto duration = std::chrono::duration_cast<TimeT>(
 				std::chrono::system_clock::now() - start);
+
 			return duration.count();
 		}
 	};
