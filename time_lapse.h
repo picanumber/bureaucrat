f27a8a444a7f6ff60438885eac374a9382843010
picanumber <picanumber@gmail.com>
2015-02-04 23:35:18 +0200

    Added the count function

--- time_lapse.h
+++ time_lapse.h
@@ -21,6 +21,10 @@ namespace tim
 	template<typename TimeT = std::chrono::milliseconds>
 	struct measure
 	{
+		/**
+		* @ fn    execution
+		* @ brief Returns the quantity (count) of the elapsed time as TimeT units
+		*/
 		template<typename F, typename ...Args>
 		static typename TimeT::rep execution(F func, Args&&... args)
 		{
@@ -32,6 +36,20 @@ namespace tim
 				std::chrono::system_clock::now() - start);
 
 			return duration.count();
+		}
+
+		/**
+		* @ fn    duration
+		* @ brief Returns the duration (in chrono's type system) of the elapsed time
+		*/
+		template<typename F, typename... Args>
+		static TimeT duration(F func, Args&&... args)
+		{
+			auto start = std::chrono::system_clock::now();
+			
+			func(std::forward<Args>(args)...);
+			
+			return std::chrono::duration_cast<TimeT>(std::chrono::system_clock::now() - start);
 		}
 	};
 }
